03a32d0033fa9a1b3388af794992460ccf3e2d1d
T3am <EngineLandTeam@gmail.com>
2020-07-20 12:01:24 -0500

    Index Assets Pages

---
+++ webpack.pages.js
@@ -0,0 +1,67 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: path.resolve(__dirname, "src/index.js"),
+  output: {
+    filename: "assets/js/bundle.js",
+  },
+  devServer: {
+    port: 5000,
+  },
+  resolve: {
+    extensions: ["*", ".mjs", ".js", ".jsx", ".svelte"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /styles\.(sa|sc|c)ss$/,
+        exclude: /node_modules/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.css$/i,
+        exclude: /styles\.css$/,
+        use: {
+          loader: "style-loader",
+          loader: "css-loader",
+          loader: "sass-loader",
+        },
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+        options: {
+          attributes: false,
+          minimize: false,
+        },
+      },
+      {
+        test: /\.svelte$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "svelte-loader",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./public/index.html",
+      filename: "../index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "assets/css/bundle.css",
+      ignoreOrder: false,
+    }),
+  ],
+};
